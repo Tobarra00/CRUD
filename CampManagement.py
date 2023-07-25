6ac65fd3c033e761c56c1052a776975c7176bb66
Tobarra00 <pedro.tobarra.leal@gmail.com>
2023-07-26 01:34:54 +0200

    Add a non funtional menu + some comments

--- CampManagement.py
+++ CampManagement.py
@@ -1,34 +1,78 @@
 from tkinter import *
 from tkinter import simpledialog, scrolledtext, filedialog
 
+
+# Declaring the root of the interface as well as some configuration
+
 root = Tk()
 root.title("Camp Data Manager")
 root.iconbitmap("CampIcon.ico")
 root.resizable(False, False)
 
-myFrame = Frame(root, height=300, width=300)
-myFrame.pack()
+# Adding a menu to the root
 
-idLabel = Label(myFrame, text="ID")
-idLabel.grid(row=1, column=0, padx=3, pady=4)
-name = Label(myFrame, text="Name")
-name.grid(row=2, column=0, padx=3, pady=4)
-surname = Label(myFrame, text="Surname")
-surname.grid(row=3, column=0, padx=3, pady=4)
-bDate = Label(myFrame, text="Birth Date")
-bDate.grid(row=4, column=0, padx=3, pady=4)
-comments = Label(myFrame, text="Comments")
-comments.grid(row=5, column=0, padx=3, pady=4)
+main_menu = Menu(root)
+root.config(menu=main_menu)
 
-createButton = Button(myFrame, text="Create", height=1, width=10)
-createButton.grid(row=6, column=0, padx=5, pady=10)
-readButton = Button(myFrame, text="Read", height=1, width=10)
-readButton.grid(row=6, column=1, padx=5, pady=10)
-updateButton = Button(myFrame, text="Update", height=1, width=10)
-updateButton.grid(row=6, column=2, padx=5, pady=10)
-deleteButton = Button(myFrame, text="Delete", height=1, width=10)
-deleteButton.grid(row=6, column=3, padx=5, pady=10)
+data_base_menu = Menu(main_menu, tearoff=0)
+data_base_menu.add_command(label="Create Data Base")
+data_base_menu.add_separator()
+data_base_menu.add_command(label="Exit")
+
+clear_data_menu = Menu(main_menu, tearoff=0)
+clear_data_menu.add_command(label="Clear info")
+
+crud_menu = Menu(main_menu, tearoff=0)
+crud_menu.add_command(label="Create")
+crud_menu.add_command(label="Read")
+crud_menu.add_command(label="Update")
+crud_menu.add_command(label="Delete")
+
+info_menu = Menu(main_menu, tearoff=0)
+info_menu.add_command(label="How to use")
+info_menu.add_command(label="About")
+
+main_menu.add_cascade(label="Data Bases", menu=data_base_menu)
+main_menu.add_cascade(label="Clear data", menu=clear_data_menu)
+main_menu.add_cascade(label="CRUD", menu=crud_menu)
+main_menu.add_cascade(label="Info", menu=info_menu)
 
 
+# Create a Frame 
 
+my_frame = Frame(root, height=300, width=300)
+my_frame.pack()
+
+# Adding text labels to the fields
+id_label = Label(my_frame, text="ID")
+id_label.grid(row=1, column=0, padx=3, pady=4)
+
+name_label = Label(my_frame, text="Name")
+name_label.grid(row=2, column=0, padx=3, pady=4)
+
+surname_label = Label(my_frame, text="Surname")
+surname_label.grid(row=3, column=0, padx=3, pady=4)
+
+birth_date_label = Label(my_frame, text="Birth Date")
+birth_date_label.grid(row=4, column=0, padx=3, pady=4)
+
+comments_label = Label(my_frame, text="Comments")
+comments_label.grid(row=5, column=0, padx=3, pady=4)
+
+
+# Adding buttons at the bottom of the interface to performe C.R.U.D.
+create_button = Button(my_frame, text="Create", height=1, width=10)
+create_button.grid(row=6, column=0, padx=5, pady=10)
+
+read_button = Button(my_frame, text="Read", height=1, width=10)
+read_button.grid(row=6, column=1, padx=5, pady=10)
+
+update_button = Button(my_frame, text="Update", height=1, width=10)
+update_button.grid(row=6, column=2, padx=5, pady=10)
+
+delete_button = Button(my_frame, text="Delete", height=1, width=10)
+delete_button.grid(row=6, column=3, padx=5, pady=10)
+
+
+# Mainloop to continuously run the application
 root.mainloop()
